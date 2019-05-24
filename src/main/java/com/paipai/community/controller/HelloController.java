69393007857b9c97285bc7aa3fa87b58ddbb7561
paipai <hxy-08@qq.com>
2019-05-24 15:12:20 +0800

    add index

--- src/main/java/com/paipai/community/controller/HelloController.java
+++ src/main/java/com/paipai/community/controller/HelloController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "hello";
+        return "index";
     }
 
 }
